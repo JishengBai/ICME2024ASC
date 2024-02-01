d7d29baf7f62e29cc3a31185727f998f33888658
haoheliu <867390095@qq.com>
2024-02-01 09:09:27 +0000

    formatting code

--- config.py
+++ config.py
@@ -11,35 +11,62 @@
 
 # Paths
 exp_id = 1
-audio_root_path = 'data'
-dev_meta_csv_path = './data/ICME2024_ASC_dev_label.csv'
-dev_fea_root_path = './data/feature/train'
-eval_meta_csv_path = './data/ICME2024_ASC_eval.csv'
-eval_fea_root_path = './data/feature/eval'
-pretrained_model_path = './pretrained/best_model.pth'
-output_path = r'./exp_{}'.format(exp_id)
+audio_root_path = "data"
+dev_meta_csv_path = "./data/ICME2024_ASC_dev_label.csv"
+dev_fea_root_path = "./data/feature/train"
+eval_meta_csv_path = "./data/ICME2024_ASC_eval.csv"
+eval_fea_root_path = "./data/feature/eval"
+pretrained_model_path = "./pretrained/best_model.pth"
+output_path = r"./exp_{}".format(exp_id)
 
 ###
-selected_scene_list = ['Bus', 'Airport', 'Metro', 'Restaurant', 
-               'Shopping mall', 'Public square', 'Urban park',
-               'Traffic street', 'Construction site', 'Bar']
-class_2_index = { 'Bus': 0, 'Airport': 1, 'Metro': 2, 'Restaurant': 3, 
-               'Shopping mall': 4, 'Public square': 5, 'Urban park': 6,
-               'Traffic street': 7, 'Construction site': 8, 'Bar': 9 }
+selected_scene_list = [
+    "Bus",
+    "Airport",
+    "Metro",
+    "Restaurant",
+    "Shopping mall",
+    "Public square",
+    "Urban park",
+    "Traffic street",
+    "Construction site",
+    "Bar",
+]
+class_2_index = {
+    "Bus": 0,
+    "Airport": 1,
+    "Metro": 2,
+    "Restaurant": 3,
+    "Shopping mall": 4,
+    "Public square": 5,
+    "Urban park": 6,
+    "Traffic street": 7,
+    "Construction site": 8,
+    "Bar": 9,
+}
 
-index_2_class = { 0: 'Bus', 1: 'Airport', 2: 'Metro', 3: 'Restaurant', 
-               4: 'Shopping mall', 5: 'Public square', 6: 'Urban park',
-               7: 'Traffic street', 8: 'Construction site', 9: 'Bar'}
+index_2_class = {
+    0: "Bus",
+    1: "Airport",
+    2: "Metro",
+    3: "Restaurant",
+    4: "Shopping mall",
+    5: "Public square",
+    6: "Urban park",
+    7: "Traffic street",
+    8: "Construction site",
+    9: "Bar",
+}
 
 # Signal Processing Setting
 sample_rate = 44100
 clip_frames = 500
 n_fft = 2048
-win_length = 1764 
-hop_length = 882  
+win_length = 1764
+hop_length = 882
 n_mels = 64
 fmin = 10
-fmax = sample_rate / 2 
+fmax = sample_rate / 2
 
 # Model Setting
 device = "cuda:0"
@@ -55,12 +82,3 @@ nhead = 8
 dim_feedforward = 32
 n_layers = 1
 dropout = 0.1
-
-
-
-
-
-
-
-
-
--- datasetup.py
+++ datasetup.py
@@ -1,9 +1,7 @@
 import os
 import urllib.request
 
-dataset_url_zenodo = [
-    "", ""
-]
+dataset_url_zenodo = ["", ""]
 
 os.makedirs("data", exist_ok=True)
 
@@ -18,4 +16,3 @@ for url in dataset_url_zenodo:
 # Unzip zip files in the ./data folder, with os.system
 os.system("unzip data/*.zip -d data/")
 os.system("python3 feature_extraction.py")
-
